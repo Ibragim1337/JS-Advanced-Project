707da7010df8d3123c59ea6745bff08a4ef96560
Alakparov Ibragim <Alakp_mr11.itstep.edu.az>
2024-01-12 22:06:32 +0400

    almost complete styles

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 let createTaskButton = document.querySelector('.create-btn');
 let hiddenMenu = document.querySelector('.main-task-container');
 let closeButton = document.querySelector('.close-btn');
+let body = document.querySelector('body')
 
 createTaskButton.onclick = function(){
   hiddenMenu.style.display = 'block';
