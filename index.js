49792d6f28ac0d5b8d6d85f1fa531b6a51869bf0
Alakparov Ibragim <Alakp_mr11.itstep.edu.az>
2024-01-12 18:43:38 +0400

    close button

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 let createTaskButton = document.querySelector('.create-btn');
-let hiddenMenu = document.querySelector('.main-task-container')
+let hiddenMenu = document.querySelector('.main-task-container');
+let closeButton = document.querySelector('.close-btn');
 
 createTaskButton.onclick = function(){
   hiddenMenu.style.display = 'block';
+}
+
+closeButton.onclick =function (){
+  hiddenMenu.style.display = 'none';
 }
