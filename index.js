f2d2f1750125f4b63266649a8e53f7778fdf9834
Alakparov Ibragim <Alakp_mr11.itstep.edu.az>
2024-01-14 00:21:01 +0400

    redact modal window

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 let createTaskButton = document.querySelector('.create-btn');
 let hiddenMenu = document.querySelector('.main-task-container');
 let closeButton = document.querySelector('.close-btn');
-let body = document.querySelector('body')
+let body = document.querySelector('body');
+let edit = document.querySelector('.edit-btn');
+let redact = document.querySelector('.redact-container');
+let closeRedact = document.querySelector('.close-redact-btn')
 
 createTaskButton.onclick = function(){
   hiddenMenu.style.display = 'block';
@@ -11,3 +14,10 @@ closeButton.onclick =function (){
   hiddenMenu.style.display = 'none';
 }
 
+edit.onclick = function() {
+  redact.style.display = 'block';
+}
+
+closeRedact.onclick = function (){
+  redact.style.display = 'none';
+}
